996f3e458212d65437279b6a9387f7d15ec9671c
levipj <levipronkjones1@gmail.com>
2024-02-23 21:32:54 +1300

    License and Entity loot tables

--- src/main/java/jammingdino/mechorigins/common/MechOrigins.java
+++ src/main/java/jammingdino/mechorigins/common/MechOrigins.java
@@ -5,13 +5,16 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class MechOrigins implements ModInitializer {
 	public static final String MOD_ID = "mechorigins";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
 	}
+
 
 }
