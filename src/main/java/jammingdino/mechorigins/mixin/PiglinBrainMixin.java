cd9487e403415d42096990c9fc80bc566c13f1c4
levipj <levipronkjones1@gmail.com>
2024-03-17 11:19:07 +1300

    Got all End cores working including recipies

--- src/main/java/jammingdino/mechorigins/mixin/PiglinBrainMixin.java
+++ src/main/java/jammingdino/mechorigins/mixin/PiglinBrainMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.PiglinBrain;
 import net.minecraft.entity.mob.PiglinEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
